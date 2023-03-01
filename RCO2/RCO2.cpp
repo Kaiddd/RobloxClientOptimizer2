ed846cb969fff152ea1e31ef72aa5df093f78ad9
Kaiddd <37759450+Kaiddd@users.noreply.github.com>
2023-03-01 00:19:20 -0500

    Add project files.

---
+++ RCO2/RCO2.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <Windows.h>
+#include <filesystem>
+#include <fstream>
+#include <thread>
+#include "curl/curl.h"
+
+//Tray icon code from stackoverflow and various other internet sources, I was unsure on how it worked.
+
+using std::string;
+
+std::string rootDir("C:\\RClientOptimizer2");
+bool isConsoleHidden = false;
+char* buf = nullptr;
+size_t sz = 0;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+LPCWSTR lpszClass = L"__hidden__";
+
+int traySystem() {
+    HINSTANCE hInstance = GetModuleHandle(nullptr);
+
+    WNDCLASS wc;
+    HWND hWnd;
+    MSG msg;
+
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hbrBackground = nullptr;
+    wc.hCursor = nullptr;
+    wc.hIcon = nullptr;
+    wc.hInstance = hInstance;
+    wc.lpfnWndProc = WndProc;
+    wc.lpszClassName = lpszClass;
+    wc.lpszMenuName = nullptr;
+    wc.style = 0;
+    RegisterClass(&wc);
+
+    hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, hInstance, nullptr);
+
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return static_cast<int>(msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+    static NOTIFYICONDATA nid;
+    std::ofstream isHiddenFile;
+
+    switch (iMsg) {
+        case WM_CREATE:
+            std::memset(&nid, 0, sizeof(nid));
+            nid.cbSize = sizeof(nid);
+            nid.hWnd = hWnd;
+            nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_INFO;
+            nid.uCallbackMessage = WM_APP + 1;
+            nid.hIcon = (HICON)LoadImageA(NULL, (rootDir + "\\animegirl.ico").c_str(), IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_SHARED);
+            memcpy_s(nid.szTip, sizeof(nid.szTip), L"RCO <3\nClick to toggle console", sizeof(wchar_t[31]));
+            Shell_NotifyIcon(NIM_ADD, &nid);
+            return 0;
+        case WM_APP + 1:
+            switch (lParam) {
+            case WM_LBUTTONDBLCLK:
+                if (isConsoleHidden) {
+                    ShowWindow(GetConsoleWindow(), SW_SHOW);
+                    isConsoleHidden = false;
+                    isHiddenFile.open(rootDir + "\\isHidden.rco");
+                    isHiddenFile << "f";
+                    isHiddenFile.close();
+                } else {
+                    ShowWindow(GetConsoleWindow(), SW_HIDE);
+                    isConsoleHidden = true;
+                    isHiddenFile.open(rootDir + "\\isHidden.rco");
+                    isHiddenFile << "t";
+                    isHiddenFile.close();
+                }
+                break;
+            case WM_LBUTTONDOWN:
+                if (isConsoleHidden) {
+                    ShowWindow(GetConsoleWindow(), SW_SHOW);
+                    isConsoleHidden = false;
+                    isHiddenFile.open(rootDir + "\\isHidden.rco");
+                    isHiddenFile << "f";
+                    isHiddenFile.close();
+                } else {
+                    ShowWindow(GetConsoleWindow(), SW_HIDE);
+                    isConsoleHidden = true;
+                    isHiddenFile.open(rootDir + "\\isHidden.rco");
+                    isHiddenFile << "t";
+                    isHiddenFile.close();
+                }
+                break;
+            }
+            break;
+        case WM_DESTROY:
+            Shell_NotifyIcon(NIM_DELETE, &nid);
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hWnd, iMsg, wParam, lParam);
+}
+
+int main() {
+    if (std::filesystem::exists(rootDir) == false) {
+        std::cout << "Could not find proper RCO files, please reinstall RCO | 0x1\n";
+        std::cin.get();
+        return 1;
+    }
+
+    if (std::filesystem::exists(rootDir + "\\animegirl.ico") == false) {
+        FILE* file;
+        if (fopen_s(&file, (rootDir + "\\animegirl.ico").c_str(), "wb") != 0) {
+            std::cout << "Could not open filepoint... | 0x4\n";
+            std::cin.get();
+            return 4;
+        }
+
+        CURL* req = curl_easy_init();
+        curl_easy_setopt(req, CURLOPT_URL, "https://raw.githubusercontent.com/Kaiddd/RobloxClientOptimizer2/main/animegirl.ico");
+        curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(req, CURLOPT_WRITEDATA, file);
+        curl_easy_perform(req);
+        curl_easy_cleanup(req);
+        
+        fclose(file);
+    }
+
+    std::thread t1(traySystem);
+
+    string robloxVersionFolder;
+    if (!(_dupenv_s(&buf, &sz, "localappdata") == 0 && buf != nullptr)) {
+        std::cout << "Error finding LocalAppData folder (NOTE: THIS PROGRAM ONLY WORKS ON WINDOWS) | 0x2\n";
+        std::cin.get();
+        return 2;
+    }
+
+    robloxVersionFolder = buf + string("\\Roblox\\Versions");
+    free(buf);
+
+    if (std::filesystem::exists(robloxVersionFolder) == false) {
+        std::cout << "Roblox not found. Please reinstall Roblox (NOTE: THIS PROGRAM ONLY WORKS ON WINDOWS) | 0x3\n";
+        std::cin.get();
+        return 3;
+    }
+
+    std::cout << robloxVersionFolder;
+    std::cin.get();
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# RCO2 is a recode of RCO
+
+(RCO and RCO2 are both made by Kaid and L8X)
+https://github.com/L8X/Roblox-Client-Optimizer
+# About RCO
+
+RCO automatically tweaks FFlags to optimize your Roblox client.
+The flags we edit are contained in our [GitHub Repository](https://github.com/Kaiddd/RobloxClientOptimizer2/blob/main/ClientAppSettings.json).
+(nul, update above link when you fork this repo.)
+Why don't you give it a try?
+
+[Click here to download RCO!](https://github.com/L8X/roblox-client-optimizer/releases/download/v1.2/RCO-Installer.zip)
+(currently 1.0, still need to make a release)
+
+Some things RCO currently does:
+
+- Unlocks FPS (For most people)
+- Optimizes Caching
+- Optimizes Graphics
+- Optimizes Textures
+- Increases user privacy
+- (May change) Disables the V3 ingame menu
+- Disables Crashpad / Backtrace crash metrics
+- Disables a large portion of client telemetry
+- Disables AbuseReportScreenshot
+- Enables font and texture item preloading
+- Enables memory prioritization and performance control
+- Enables FULL graphics quality options (21 instead of 10!)
+- Enables QuickGameLaunch
+- Enables ParallelHumanoidManager
+- Enables BatchAssetApi
+
+Do you have any issues installing RCO? Read here!
+1. RCO *ONLY* works on Windows, though it should also work on custom Windows mods. (Only tested on 10)
+2. If you have UAC disabled, or you are using a custom Windows ISO that disables UACs functionality, run rco-installer-NOUAC-DONT-USE-UNLESS-NEEDED.cmd to install RCO, instead of the regular installer.
